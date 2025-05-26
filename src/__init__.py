216a67f675dae1d9c12586f0f8ea90df6e245fa2
dinesh <rijaldinesh618@gmail.com>
2025-05-26 20:26:59 +0545

    Config and other codes are added

--- src/__init__.py
+++ src/__init__.py
@@ -1,15 +1,21 @@
 from fastapi import FastAPI
 from src.books.routes import book_router
 from fastapi.middleware.cors import CORSMiddleware
+from contextlib import asynccontextmanager
 
-# Just Commenting Nothing to check the git commit
+@asynccontextmanager
+async def life_span(app:FastAPI):
+    print(f"Server is Starting...")
+    yield
+    print(f"Server has been Stopped..")
 
 version = 'v1'
 
 app = FastAPI(
     title= 'Bookly',
     description= 'A REST API Service for book review',
-    version= version
+    version= version,
+    lifespan=life_span
 )
 app.add_middleware(CORSMiddleware, allow_origins=["*"],allow_methods=["*"], allow_headers=["*"])
 
