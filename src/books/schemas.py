41f40875ead9fcf46ef3b30961dfc033e232b4e9
dinesh <rijaldinesh618@gmail.com>
2025-06-04 07:39:22 +0545

    Connected to DataBase and Everything Works Grate

--- src/books/schemas.py
+++ src/books/schemas.py
@@ -1,8 +1,20 @@
 from pydantic import BaseModel
+import uuid
+from datetime import datetime, date
 
 
 class Books(BaseModel):
-    id: int
+    uid: uuid.UUID
+    title : str
+    author : str
+    publisher : str
+    published_date : date
+    page : int
+    language : str
+    created_at : datetime
+    update_at : datetime
+
+class BookCreateMOdel(BaseModel):
     title : str
     author : str
     publisher : str
@@ -14,6 +26,5 @@ class BookUpdateModel(BaseModel):
     title : str
     author : str
     publisher : str
-    published_date : str
     page : int
     language : str
--- src/db/main.py
+++ src/db/main.py
@@ -1,14 +1,28 @@
 from sqlmodel import create_engine, text, SQLModel
 from sqlalchemy.ext.asyncio import AsyncEngine
+from sqlmodel.ext.asyncio.session import AsyncSession
+from sqlalchemy.ext.asyncio import async_sessionmaker
+from typing import AsyncGenerator
 from src.books.models import Book
 from src.config import Config
 
-engine = AsyncEngine(
+
+async_engine = AsyncEngine(
     create_engine(
     url=Config.DATABASE_URL,
     echo=True
 ))
 
 async def init_db():
-    async with engine.begin() as conn:
+    async with async_engine.begin() as conn:
         await conn.run_sync(SQLModel.metadata.create_all)
+
+
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
+    Session = async_sessionmaker(
+        bind=async_engine,
+        class_=AsyncSession,
+        expire_on_commit=False
+    )
+    async with Session() as session:
+        yield session
