52a2027b02acdbfa63668309ee6185b62d4b3766
dinesh <rijaldinesh618@gmail.com>
2025-06-05 08:38:04 +0545

    Added auth and created a user sucessfully

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from src.books.routes import book_router
+from src.auth.routes import auth_router
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from src.db.main import init_db
@@ -22,3 +23,4 @@ app = FastAPI(
 app.add_middleware(CORSMiddleware, allow_origins=["*"],allow_methods=["*"], allow_headers=["*"])
 
 app.include_router(book_router, prefix=f'/api/{version}/books', tags=['books'])
+app.include_router(auth_router, prefix=f'/api/{version}/auth', tags=['auth'])
