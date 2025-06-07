0f94e9a268f14e002a203b0ffbaefd93ddd24455
dinesh <rijaldinesh618@gmail.com>
2025-06-07 07:44:31 +0545

    HTTP Bearer Authentication

---
+++ src/auth/dependencies.py
@@ -0,0 +1,48 @@
+from fastapi.security import HTTPBearer
+from fastapi import Request, status
+from fastapi.security.http import HTTPAuthorizationCredentials
+from fastapi.exceptions import HTTPException
+from .utils import decode_token
+
+class AccessTokenBearer(HTTPBearer):
+    
+    def __init__(self, auto_error: bool = True):
+        super().__init__(auto_error=auto_error)
+
+    async def __call__(self, request: Request) -> HTTPAuthorizationCredentials | None:
+        creds = await super().__call__(request)
+
+        if creds is not None:
+            token = creds.credentials
+            token_data = decode_token(token)
+
+            if token_data is not None:
+                if token_data['refresh']:
+                    raise HTTPException(
+                        status_code=status.HTTP_403_FORBIDDEN,
+                        detail="Provide an access token"
+                    )
+                if not self.token_valid:
+                    raise HTTPException(
+                        status_code=status.HTTP_403_FORBIDDEN,
+                        detail="Invalid or expired token"
+                    )
+                return token_data
+                
+            else:
+                raise HTTPException(
+                        status_code=status.HTTP_403_FORBIDDEN,
+                        detail="Token is None"
+                    )
+        
+        else:
+            raise HTTPException(
+                        status_code=status.HTTP_403_FORBIDDEN,
+                        detail="Creds is None"
+                    )
+
+    
+    def token_valid(self, token : str) -> bool :
+        token_data = decode_token(token)
+
+        return True if token_data is not None else False
--- src/books/routes.py
+++ src/books/routes.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, status, Depends
 from fastapi.exceptions import HTTPException
 from typing import List
 from .service import BookService
+from src.auth.dependencies import AccessTokenBearer
 from src.books.schemas import Books, BookUpdateModel, BookCreateMOdel
 from sqlmodel.ext.asyncio.session import AsyncSession
 from src.db.main import get_session
@@ -9,20 +10,24 @@ from src.db.main import get_session
 
 book_router = APIRouter()
 book_service = BookService()
+access_token_bearer = AccessTokenBearer()
 
 
 @book_router.get('/', response_model=List[Books])
-async def get_all_books(session:AsyncSession = Depends(get_session)) :
+async def get_all_books(session:AsyncSession = Depends(get_session), user_details = Depends(access_token_bearer)) :
     books =await book_service.get_all_books(session)
     return books
 
 @book_router.post('/', status_code=status.HTTP_201_CREATED, response_model=Books)
-async def create_a_book(book_data:BookCreateMOdel, session:AsyncSession = Depends(get_session)) :
+async def create_a_book(book_data:BookCreateMOdel,
+    session:AsyncSession = Depends(get_session),
+    user_details = Depends(access_token_bearer)
+) :
     new_book =await book_service.create_book(book_data, session)
     return new_book
 
 @book_router.get('/{book_uid}', response_model=Books)
-async def get_book(book_uid:str, session:AsyncSession = Depends(get_session)) :
+async def get_book(book_uid:str, session:AsyncSession = Depends(get_session), user_details = Depends(access_token_bearer)) :
     book =await book_service.get_book(book_uid, session)
     if book is  None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Book not found")
@@ -31,7 +36,10 @@ async def get_book(book_uid:str, session:AsyncSession = Depends(get_session)) :
 
 
 @book_router.patch('/{book_uid}', response_model=Books)
-async def update_book(book_uid:str, book_update_data:BookUpdateModel, session:AsyncSession = Depends(get_session)) :
+async def update_book(book_uid:str, book_update_data:BookUpdateModel, 
+    session:AsyncSession = Depends(get_session),
+    user_details = Depends(access_token_bearer)
+) :
     updated_book =await book_service.update_book(book_uid,book_update_data, session)
     if updated_book is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Book not found")
@@ -41,7 +49,7 @@ async def update_book(book_uid:str, book_update_data:BookUpdateModel, session:As
 
 
 @book_router.delete('/{book_uid}', status_code=status.HTTP_204_NO_CONTENT)
-async def delete_book(book_uid:str, session:AsyncSession = Depends(get_session)) :
+async def delete_book(book_uid:str, session:AsyncSession = Depends(get_session), user_details = Depends(access_token_bearer)) :
     book_to_delete = await book_service.delete_book(book_uid, session)
     if book_to_delete is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Book not found")
